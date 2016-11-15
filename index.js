34dafd695f978d96fed285dd48a8d91d54f3b9f0
MSturges <Sturges3232@gmail.com>
2016-11-15 09:05:15 -0700

    express set up, pre mongoose

---
+++ index.js
@@ -0,0 +1,32 @@
+// Main starting point of out server
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// making an instance of express
+const app = express();
+
+const router = require('./router')
+
+router(app);
+
+
+// App SetUp
+
+// middleware in express -
+// any request will be sent throught them
+
+// morgan is a logging frame work: mostly for debuggin
+app.use(morgan('combined'));
+// bodyParser will parse incoming request:
+// parsed as though it was JSON
+app.use(bodyParser.json({ type: '*/*'}));
+
+// Server SetUp
+const port = process.env.PORT || 3090;
+
+// create an http server and push it to our app
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on', port);
